4357ba456280cc4bed06386e00f858d1a8bf4d8e
AdrianeDriane <dilaoaparre@gmail.com>
2025-12-08 13:06:50 +0800

    feat: initial router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+// Pages
 import { LandingPage } from "./pages/LandingPage";
+import { LoginPage } from "./components/registerBarangayPage/LoginPage";
+import { RegisterBarangayPage } from "./pages/RegisterBarangayPage";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Routes>
+      {/* Landing Page */}
+      <Route path="/" element={<LandingPage />} />
+
+      {/* Register Barangay */}
+      <Route path="/register" element={<RegisterBarangayPage />} />
+
+      {/* Login */}
+      <Route path="/login" element={<LoginPage />} />
+
+      {/* Default route (change as needed) */}
+      {/* <Route path="*" element={<RegisterBarangayPage />} /> */}
+    </Routes>
+  );
 }
+
 export default App;
 
